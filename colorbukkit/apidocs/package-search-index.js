b8c71356acccecce44b80eca658d191b7aa530a1
Pheotis <49039536+Pheotis@users.noreply.github.com>
2023-09-06 15:35:53 -0400

    Added colorbukkit javadocs.

---
+++ colorbukkit/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"dev.thorinwasher.utils.colorbukkit"},{"l":"dev.thorinwasher.utils.colorbukkit.utils"}];updateSearchResults();
